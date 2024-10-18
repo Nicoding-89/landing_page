a6e5d655f7c5669e0f0ee8affca9bc140568d0fe
nicoding89 <ps.nicolas.labarca@gmail.com>
2024-10-17 22:37:22 -0300

    Add project basic structure

---
+++ landing_page/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>WELCOME!</h1>
+      <p></p>
+      <button><a></a></button>
+    </div>
+  )
+}
+
+export default Hero
