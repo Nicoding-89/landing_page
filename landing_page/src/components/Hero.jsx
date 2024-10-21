bcd8c202165724402216bb158056ff82ef79d6df
Nicoding-89 <ps.nicolas.labarca@gmail.com>
2024-10-21 01:04:13 -0300

    feat: add header component
    - Implemented a responsive header with navigation links
    - Included branding logo and title
    - Added its styles

--- landing_page/src/components/Hero.jsx
+++ landing_page/src/components/Hero.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import '../styles/Hero.css'
+import heroImg from '../assets/img/hero-img.webp'
 
 const Hero = () => {
   return (
-    <div>
-      <h1>WELCOME!</h1>
-      <p></p>
-      <button><a></a></button>
-    </div>
+    <section className='hero'>
+      <div className='hero-text'>
+        <h1 className='hero-title'>WELCOME!</h1>
+        <h2 className='hero-subtitle'>I'm a junior full stack JavaScript Web Developer</h2>
+        <p className='hero-description'>I aim to design web projects that not only function well but also resonate with users through a blend of simplicity and elegance.</p>
+        <button className='btn btn-primary cta-btn'><a>Projects</a></button>
+      </div>
+      <div className='hero-img-container'>
+        <img src={heroImg} className="img-fluid rounded hero-img" alt="A programmer coding"></img>
+      </div>
+    </section>
   )
 }
 
