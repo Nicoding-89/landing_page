2ee2b96fd6b82a2f2d6fbbfc1120cdc40f596c5a
Nicoding-89 <ps.nicolas.labarca@gmail.com>
2024-11-22 17:45:34 -0300

    Refactor styles across multiple components for improved layout and design

--- landing_page/src/components/Hero.jsx
+++ landing_page/src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1 className='hero-title'>WELCOME!</h1>
         <h2 className='hero-subtitle'>I'm a junior full stack JavaScript Web Developer</h2>
         <p className='hero-description'>I aim to design web projects that not only function well but also resonate with users through a blend of simplicity and elegance.</p>
-        <button className='btn btn-primary cta-btn'><a href='#projects' style={{color: '#ffffff'}}>Projects</a></button>
+        <a href='#projects' className='btn btn-primary cta-btn'>Projects</a>
       </div>
       <div className='hero-img-container'>
         <img src={heroImg} className="img-fluid rounded hero-img" alt="A programmer coding"></img>
