66bbdcaa0d039ce5b79e4b8632c9a303af44033f
Nicoding-89 <112462063+Nicoding-89@users.noreply.github.com>
2024-11-22 21:51:27 -0300

    Remove code no longer needed after refactoring

--- landing_page/src/components/ValidationMsg.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ValidationMessage = ({ id, style, message }) => (
-  <p id={id} className={style} aria-live="assertive">
-    {message}
-  </p>
-);
-
-export default ValidationMessage
